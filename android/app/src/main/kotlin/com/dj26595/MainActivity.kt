936dbe6ae9cdabfc1a9f98b3f4efe5fe08faad3d
David <jadczyk.develop@gmail.com>
2024-07-21 15:06:56 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/dj26595/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dj26595.rag_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:rag_app/app/app.dart';
+import 'package:rag_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
